741d2776c8e89b801e86d808d2ccdb36c94a673b
Ryan Dalby <dalbyryan3@gmail.com>
2021-09-20 13:57:07 -0600

    Finished rough impelmentation of ID3

--- DecisionTree/decision_tree.py
+++ DecisionTree/decision_tree.py
@@ -1,4 +1,5 @@
 import math
+from os import name
 # car_test_data = pd.read_csv('car/test.csv', )
 # car_train_data = pd.read_csv('car/train.csv')
 
@@ -40,14 +41,65 @@ class Node:
                 self.children.append(child)
                 self.weights.append(weight)
                 child.parent = self
+    def node_depth(self):
+        depth = 0
+        current_node = self
+        while (current_node.parent is not None):
+            depth += 1
+            current_node = current_node.parent
+        return depth
 
 class DecisionTree:
-    def __init__(self):
-        self.root = Node('root')
+    def __init__(self, attribute_possible_vals):
+        # A dictionary mapping from keys of all potential attributes to a list of all possible values for that attribute
+
+        self.attribute_possible_vals = attribute_possible_vals
+        # self.root = Node('root')
         # self.current_node = self.root
 
-    @staticmethod
-    def entropy(labels):
+    def ID3_train(self, S, attributes, labels, metric=None, max_depth=None):
+        if len(S) <= 0 or len(labels) != len(S):
+            raise Exception('Must have at least a single example and label and they must be the be same length')
+
+        # All examples have same label:
+        if(labels.count(labels[0]) == len(labels)):
+            return Node(labels[0])# leaf node with the label
+        # Attributes empty:
+        if(len(attributes) <= 0):
+            possible_labels = set(labels)
+            most_common_label = max(possible_labels, key=labels.count)
+            return Node(most_common_label)
+        # Otherwise:
+        (best_attribute, Sv_dict, _) = self.find_best_attribute(S, labels, attributes, metric=metric)
+        root_node = Node(best_attribute)
+        current_depth = self.number_of_attribute_splits(attributes)
+        stop_tree_growth = False
+        if(max_depth is not None and current_depth+1 >= max_depth):
+            # If next depth is max depth then we set flag to stop tree growth
+            stop_tree_growth = True
+        for value in self.attribute_possible_vals[best_attribute]:
+            if (value not in Sv_dict):
+                possible_labels = set(labels)
+                most_common_label = max(possible_labels, key=labels.count)
+                root_node.add_child(Node(most_common_label), value)
+            else:
+                Sv_tuple = Sv_dict[value]
+                Sv = Sv_tuple[0]
+                labels_v = Sv_tuple[1]
+                if (stop_tree_growth):
+                    # To stop tree growth we set the labels to the most common value so next recursion returns Node(most common value)
+                    possible_labels = set(labels_v)
+                    most_common_label = max(possible_labels, key=labels_v.count)
+                    labels_v = [most_common_label] * len(labels_v)
+                reduced_attributes = set(attributes) - set([best_attribute])
+                root_node.add_child(self.ID3_train(Sv, reduced_attributes, labels_v, metric=metric, max_depth=max_depth), value)
+        return root_node
+    
+    def number_of_attribute_splits(self, current_attributes):
+        return len(self.attribute_possible_vals.keys()) - len(current_attributes)
+
+    @classmethod
+    def entropy(cls, labels):
         N = len(labels)
         possible_labels = set(labels)
         entropy = 0
@@ -58,8 +110,8 @@ class DecisionTree:
                 entropy += -label_prob*math.log(label_prob)
         return entropy
 
-    @staticmethod
-    def majority_error(labels):
+    @classmethod
+    def majority_error(cls, labels):
         N = len(labels)
         possible_labels = set(labels)
         most_common_label = max(possible_labels, key=labels.count)
@@ -69,8 +121,8 @@ class DecisionTree:
             non_most_common_count += labels.count(label)
         return non_most_common_count/N
 
-    @staticmethod
-    def gini_index(labels):
+    @classmethod
+    def gini_index(cls, labels):
         N = len(labels)
         possible_labels = set(labels)
         label_prob_sum = 0
@@ -80,29 +132,31 @@ class DecisionTree:
             label_prob_sum += label_prob**2 
         return 1-label_prob_sum
 
-    @staticmethod
-    def gain(S, labels, attribute, metric=entropy):
+    @classmethod
+    def gain(cls, S, labels, attribute, metric=None):
+        if metric is None:
+            metric = cls.entropy
         N = len(S)
         S_metric = metric(labels)
         expected_metric_sum = 0
-        Sv_dict = DecisionTree.get_value_subset(S, labels, attribute)
+        Sv_dict = cls.get_value_subset(S, labels, attribute)
         for Sv_key in Sv_dict.keys():
-            Sv = Sv_dict[Sv_key]
-            labels_v = [i[1] for i in Sv]
+            (Sv, labels_v) = Sv_dict[Sv_key]
             expected_metric_sum += (len(Sv)/N) * metric(labels_v)
-        # Returns gain value and the splitted subsets from splitting on attribute
+        # Returns gain value and a dictionary that given a value of the attribute maps to a tuple of (Sv, labels_v) representing the subset of S all containing the same value of the attribute
         return (S_metric - expected_metric_sum, Sv_dict)
 
-    @staticmethod
-    def get_attribute_values(S, attribute):
+    @classmethod
+    def get_attribute_values(cls, S, attribute):
+        # Should likely just use get_value_subset and look at keys but this may be faster if subset isn't needed
         attribute_vals = set()
         for example in S:
             ex_val = example[attribute]
             attribute_vals.add(ex_val)
         return attribute_vals
 
-    @staticmethod
-    def get_value_subset(S, labels, attribute):
+    @classmethod
+    def get_value_subset(cls, S, labels, attribute):
         if (len(S) != len(labels)):
             raise Exception('S must be the same length as corresponding labels')
         Sv_dict = {}
@@ -111,46 +165,26 @@ class DecisionTree:
             label = labels[i]
             ex_val = example[attribute]
             if ex_val in Sv_dict:
-                Sv_dict[ex_val].append((example, label))
+                Sv_dict[ex_val][0].append(example)
+                Sv_dict[ex_val][1].append(label)
             else:
-                Sv_dict[ex_val] = [(example, label)]
+                Sv_dict[ex_val] = ([example],[label])
+        # a dictionary that given a value of the attribute maps to a tuple of (Sv, labels_v) representing the subset of S all containing the same value of the attribute
         return Sv_dict
 
-
-
-    def ID3_train(self, S, attributes, labels, max_depth=None):
-        pass
-
-        # if len(labels) <= 0:
-        #     raise Exception('Must have at least a single example or label or attribute')
-
-        # # All examples have same label
-        # if(labels.count(labels[0]) == len(labels)):
-        #     return
-        # # Attributes empty
-        # if(len(attributes) <= 0):
-        #     return
-
-        # current_node = Node('root')
-
-        # best_attr = self.find_best_attr(S, 'entropy')
-
-        # current_node.name = best_attr
-
-        # best_attr_vals = set()
-        # for example in S:
-        #     best_attr_vals.add(example[best_attr])
-        # for val in best_attr_vals:
-        #     branch = Node(val)
-        #     current_node.add_child(Node())
-
-        
-
-
-
-
-
-# class DecisionTree:
-#     _root
-
-    
+    @classmethod
+    def find_best_attribute(cls, S, labels, attributes, metric=None):
+        if metric is None:
+            metric = cls.entropy
+        best_Sv_dict = None 
+        best_attribute = None
+        best_gain = 0
+        for current_attribute in attributes:
+            (current_gain, current_Sv_dict) = cls.gain(S, labels, current_attribute, metric=metric)
+            # Will choose best gain based on this equality, if multiple gains of same value will arbitrarily choose one
+            if(current_gain > best_gain):
+                best_Sv_dict = current_Sv_dict
+                best_attribute = current_attribute
+                best_gain = current_gain
+        # Returns a tuple of the best attribute, a dictionary maps to a tuple of (Sv, labels_v) representing the subsets of S for splitting on the best attribute, and the best gain value
+        return (best_attribute, best_Sv_dict, best_gain)
--- DecisionTree/decision_tree_test.py
+++ DecisionTree/decision_tree_test.py
@@ -1,29 +1,73 @@
-from logging import PlaceHolder
+from logging import root
+from os import WSTOPPED
 import unittest
-from decision_tree import DecisionTree
+from decision_tree import DecisionTree, Node
 
 class DecisionTreeTester(unittest.TestCase):
+    # Helper and setup methods
     def setUp(self):
+        self.attributes = ['Outlook', 'Temperature', 'Humidity', 'Wind']
+        self.attribute_possible_vals = {
+            self.attributes[0] : ['Sunny', 'Overcast', 'Rainy'],
+            self.attributes[1] : ['Hot', 'Medium', 'Cold'],
+            self.attributes[2] : ['High', 'Normal', 'Low'],
+            self.attributes[3] : ['Strong', 'Weak']
+        }
         self.labels = [False,False,True,True,True,False,True,False,True,True,True,True,True,False]
         self.S = [
-            self.be('Sunny','Hot', 'High', 'Weak'),
-            self.be('Sunny','Hot', 'High', 'Strong'),
-            self.be('Overcast','Hot', 'High', 'Weak'),
-            self.be('Rainy','Medium', 'High', 'Weak'),
-            self.be('Rainy','Cold', 'Normal', 'Weak'),
-            self.be('Rainy','Cold', 'Normal', 'Strong'),
-            self.be('Overcast','Cold', 'Normal', 'Strong'),
-            self.be('Sunny','Medium', 'High', 'Weak'),
-            self.be('Sunny','Cold', 'Normal', 'Weak'),
-            self.be('Rainy','Medium', 'Normal', 'Weak'),
-            self.be('Sunny','Medium', 'Normal', 'Strong'),
-            self.be('Overcast','Medium', 'High', 'Strong'),
-            self.be('Overcast','Hot', 'Normal', 'Weak'),
-            self.be('Rainy','Medium', 'High', 'Strong')
+            self.build_example('Sunny','Hot', 'High', 'Weak'),
+            self.build_example('Sunny','Hot', 'High', 'Strong'),
+            self.build_example('Overcast','Hot', 'High', 'Weak'),
+            self.build_example('Rainy','Medium', 'High', 'Weak'),
+            self.build_example('Rainy','Cold', 'Normal', 'Weak'),
+            self.build_example('Rainy','Cold', 'Normal', 'Strong'),
+            self.build_example('Overcast','Cold', 'Normal', 'Strong'),
+            self.build_example('Sunny','Medium', 'High', 'Weak'),
+            self.build_example('Sunny','Cold', 'Normal', 'Weak'),
+            self.build_example('Rainy','Medium', 'Normal', 'Weak'),
+            self.build_example('Sunny','Medium', 'Normal', 'Strong'),
+            self.build_example('Overcast','Medium', 'High', 'Strong'),
+            self.build_example('Overcast','Hot', 'Normal', 'Weak'),
+            self.build_example('Rainy','Medium', 'High', 'Strong')
         ]
-    def be(self, outlook, temperature, humidity, wind):
+    def build_example(self, outlook, temperature, humidity, wind):
         # Build example
-        return {'Outlook':outlook, 'Temperature':temperature,'Humidity':humidity, 'Wind':wind}
+        return {self.attributes[0]:outlook, self.attributes[1]:temperature,self.attributes[2]:humidity, self.attributes[3]:wind}
+
+    def visualize_tree(self, root_node, should_print=False):
+        tree_str = {} 
+
+        # Essentially BFS
+        queue = []
+        visited = []
+        queue.append((root_node,''))
+        visited.append(root_node)
+        while queue:
+            current_node_tuple = queue.pop(0)
+            current_node = current_node_tuple[0]
+            current_node_name = str(current_node.name)
+            current_node_weight = str(current_node_tuple[1])
+            current_depth = current_node.node_depth()
+            if current_depth in tree_str:
+                tree_str[current_depth] += (current_node_weight + '-->' + current_node_name + '    ')
+            else:
+                tree_str[current_depth] = (current_node_weight + '-->' + current_node_name + '    ')
+            for i in range(len(current_node.children)):
+                child = current_node.children[i]
+                weight = current_node.weights[i]
+                if(child not in visited):
+                    queue.append((child, weight))
+                    visited.append(child)
+        if (should_print):
+            print()
+            print('Visualize tree: ')
+            for key in tree_str.keys():
+                print(tree_str[key])
+            print('End visualization ')
+            print()
+        return tree_str
+        
+    # Testing methods
     def test_entropy(self):
         self.assertAlmostEqual(DecisionTree.entropy(self.labels), 0.652, places=3)
     def test_majority_error(self):
@@ -42,25 +86,16 @@ class DecisionTreeTester(unittest.TestCase):
         self.assertSetEqual(set(DecisionTree.get_value_subset(self.S, self.labels, 'Wind').keys()), {'Strong','Weak'})
 
         S_outlook = DecisionTree.get_value_subset(self.S, self.labels, 'Outlook')
-        sunny_label_vals = []
-        S_sunny = S_outlook['Sunny']
-        overcast_label_vals = []
-        S_overcast = S_outlook['Overcast']
-        rainy_label_vals = []
-        S_rainy = S_outlook['Rainy']
-        for label in S_sunny:
-            sunny_label_vals.append(label[1])
+        sunny_label_vals = S_outlook['Sunny'][1]
         self.assertListEqual(sunny_label_vals, [False, False, False, True, True])
-        for label in S_overcast:
-            overcast_label_vals.append(label[1])
+        overcast_label_vals = S_outlook['Overcast'][1]
         self.assertListEqual(overcast_label_vals, [True, True, True, True])
-        for label in S_rainy:
-            rainy_label_vals.append(label[1])
+        rainy_label_vals = S_outlook['Rainy'][1]
         self.assertListEqual(rainy_label_vals, [True, True, False, True, False])
 
     def test_gain(self):
-        self.assertAlmostEqual(DecisionTree.gain(self.S, self.labels, 'Outlook', metric=DecisionTree.entropy)[0], 0.171, places=3)
-        self.assertAlmostEqual(DecisionTree.gain(self.S, self.labels, 'Temperature', metric=DecisionTree.entropy)[0], 0.020, places=3)
+        self.assertAlmostEqual(DecisionTree.gain(self.S, self.labels, 'Outlook')[0], 0.171, places=3)
+        self.assertAlmostEqual(DecisionTree.gain(self.S, self.labels, 'Temperature')[0], 0.020, places=3)
         self.assertAlmostEqual(DecisionTree.gain(self.S, self.labels, 'Humidity', metric=DecisionTree.entropy)[0], 0.105, places=3)
         self.assertAlmostEqual(DecisionTree.gain(self.S, self.labels, 'Wind', metric=DecisionTree.entropy)[0], 0.0334, places=3)
 
@@ -74,5 +109,42 @@ class DecisionTreeTester(unittest.TestCase):
         self.assertAlmostEqual(DecisionTree.gain(self.S, self.labels, 'Humidity', metric=DecisionTree.gini_index)[0], 0.092, places=3)
         self.assertAlmostEqual(DecisionTree.gain(self.S, self.labels, 'Wind', metric=DecisionTree.gini_index)[0], 0.031, places=3)
 
+    def test_find_best_attribute(self):
+        (best_attr, Sv_dict, best_gain) = DecisionTree.find_best_attribute(self.S, self.labels, self.attributes)
+        self.assertEqual(best_attr, 'Outlook')
+        self.assertAlmostEqual(best_gain, 0.171, places=3)
+        sunny_label_vals = Sv_dict['Sunny'][1]
+        self.assertListEqual(sunny_label_vals, [False, False, False, True, True])
+        overcast_label_vals = Sv_dict['Overcast'][1]
+        self.assertListEqual(overcast_label_vals, [True, True, True, True])
+        rainy_label_vals = Sv_dict['Rainy'][1]
+        self.assertListEqual(rainy_label_vals, [True, True, False, True, False])
+
+        (best_attr, Sv_dict, best_gain) = DecisionTree.find_best_attribute(self.S, self.labels, self.attributes, metric=DecisionTree.majority_error)
+        self.assertTrue(best_attr == 'Outlook' or best_attr =='Humidity')
+        self.assertAlmostEqual(best_gain, 0.0714, places=3)
+
+        (best_attr, Sv_dict, best_gain) = DecisionTree.find_best_attribute(self.S, self.labels, self.attributes, metric=DecisionTree.gini_index)
+        self.assertEqual(best_attr, 'Outlook')
+        self.assertAlmostEqual(best_gain, 0.116, places=3)
+        sunny_label_vals = Sv_dict['Sunny'][1]
+        self.assertListEqual(sunny_label_vals, [False, False, False, True, True])
+        overcast_label_vals = Sv_dict['Overcast'][1]
+        self.assertListEqual(overcast_label_vals, [True, True, True, True])
+        rainy_label_vals = Sv_dict['Rainy'][1]
+        self.assertListEqual(rainy_label_vals, [True, True, False, True, False])
+    
+    def test_ID3Train(self):
+        dc = DecisionTree(self.attribute_possible_vals)
+        root_node = dc.ID3_train(self.S, self.attributes, self.labels)
+        tree_str = self.visualize_tree(root_node)
+        self.assertEqual(tree_str[0], '-->Outlook    ')
+        self.assertEqual(tree_str[1], 'Sunny-->Humidity    Overcast-->True    Rainy-->Wind    ')
+        self.assertEqual(tree_str[2], 'High-->False    Normal-->True    Low-->False    Strong-->False    Weak-->True    ')
+
+        root_node = dc.ID3_train(self.S, self.attributes, self.labels, max_depth=1)
+        tree_str = self.visualize_tree(root_node)
+        self.assertEqual(tree_str[0], '-->Outlook    ')
+        self.assertEqual(tree_str[1], 'Sunny-->False    Overcast-->True    Rainy-->True    ')
 
 unittest.main(argv=[''], verbosity=2, exit=False)
