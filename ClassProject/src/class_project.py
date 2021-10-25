91fe798033d565e00c90ec7fe68d0d0de44b45cc
Ryan Dalby <dalbyryan3@gmail.com>
2021-10-24 20:26:20 -0600

    Logisitic regression evaluation

--- ClassProject/src/class_project.py
+++ ClassProject/src/class_project.py
@@ -1,14 +1,15 @@
 # %%
 import pickle
-from datetime import date, datetime
+from datetime import datetime
 import numpy as np
 import pandas as pd
 from pathlib import Path
-from pandas.core.arrays import categorical
 from sklearn import preprocessing
 from sklearn import linear_model
-from sklearn import svm
 from sklearn import model_selection
+from sklearn import metrics
+from sklearn import svm
+import matplotlib.pyplot as plt
 
 # %% Config variables
 data_root_path = Path('../data')
@@ -23,6 +24,7 @@ def output_kaggle_prediction_and_save_model(model, X_final_test, final_idxs):
     final_test_pred_df.to_csv(kaggle_submissions_path / '{0}'.format(filename+'.csv'), index=False)
     with open(models_path / '{0}'.format(filename+'.pkl'),'wb') as f:
         pickle.dump(model,f)
+    return filename
 
 def encode_data(X, categorical_cols_idxs, enc):
     X_categorical = X[:,categorical_cols_idxs]
@@ -30,6 +32,38 @@ def encode_data(X, categorical_cols_idxs, enc):
     X_categorical_enc = enc.transform(X_categorical)
     X_enc = np.concatenate((X_non_categorical,X_categorical_enc), axis=1)
     return X_enc
+
+def evaluate_model(model, X_train, y_train, X_test, y_test, model_name_str='', should_print=True):
+    # Evaluates model in terms of test and train accuracy 
+    # Train data evaluation
+    y_train_pred = model.predict(X_train)
+    train_acc = model.score(X_train, y_train)
+    train_auc = metrics.roc_auc_score(y_train, y_train_pred)
+
+    # Test data evaluation
+    y_test_pred = model.predict(X_test)
+    test_acc = model.score(X_test, y_test)
+    test_auc = metrics.roc_auc_score(y_test, y_test_pred)
+
+    if should_print:
+        print('{0} model: train accuracy = {1}, train AUC = {2} \ntest accuracy = {3}, test AUC = {4}'.format(model_name_str, train_acc, train_auc, test_acc, test_auc))
+    return train_acc, train_auc, test_acc, test_auc
+
+# Learning curve
+def create_learning_curves(model, X, y, num_data_points=5, model_name_str=''):
+    num_samples_vals, train_scores_5cv, val_scores_5cv = model_selection.learning_curve(model, X, y, train_sizes=np.linspace(0.1, 1.0, num_data_points))
+    # Get mean of 5-fold cross validation
+    avg_train_scores = np.mean(train_scores_5cv, axis=1)
+    avg_val_scores = np.mean(val_scores_5cv, axis=1)
+    plt.figure()
+    plt.plot(num_samples_vals, avg_train_scores, label='Training score')
+    plt.plot(num_samples_vals, avg_val_scores, label='Validation score')
+    plt.legend()
+    plt.xlabel('Number of examples')
+    plt.ylabel('Score (accuracy)')
+    plt.title('Learning Curves for {0}'.format(model_name_str))
+    plt.show()
+    return avg_train_scores, avg_val_scores 
 # %%
 # Load path 
 loaded_train_data = pd.read_csv(data_root_path / 'train_final.csv')
@@ -53,26 +87,66 @@ X_final_test_enc = encode_data(X_final_test, categorical_cols_idxs, enc)
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X_full_train_enc, y_full_train, test_size=0.25)
 
 # %%
+###
+# %%
 # Logistic regression
-log_model = linear_model.SGDClassifier(loss='log')
+log_model = linear_model.LogisticRegression(solver='lbfgs', max_iter=500) 
+log_model_name_str = 'Logistic Regression Classification'
+# log_model = linear_model.SGDClassifier(loss='log') 
+# SGD fails to converge to good optima sometimes, so lbfgs is used which finds optima without being too computationally expensive for this data
 log_model.fit(X_train, y_train)
-train_acc = log_model.score(X_train, y_train)
-test_acc = log_model.score(X_test, y_test)
-print('Logistic regression: test accuracy = {0}, train accuracy = {1}'.format(train_acc, test_acc))
+
+# Evaluate logistic regression
+log_model_eval = evaluate_model(log_model, X_train, y_train, X_test, y_test, model_name_str=log_model_name_str)
+
+# %% Learning curves 
+log_model_train_learning_curve, log_model_val_learning_curve = create_learning_curves(log_model, X_train, y_train, model_name_str=log_model_name_str, num_data_points=100)
+
+# %% Save learning curves 
+with open( '{0}'.format('log_learning_curve.pkl'),'wb') as f:
+    pickle.dump([log_model_train_learning_curve, log_model_val_learning_curve],f)
 
 # %% 
 # Output prediction and save model
 output_kaggle_prediction_and_save_model(log_model, X_final_test_enc, final_idxs)
 
 # %%
+###
+# %%
 # Linear support vector machine classification 
-svc_model = linear_model.SGDClassifier(loss='hinge')
+svc_model = svm.SVC(kernel='linear', verbose=True)
 svc_model.fit(X_train, y_train)
-train_acc = svc_model.score(X_train, y_train)
-test_acc = svc_model.score(X_test, y_test)
-print('Linear support vector classification: test accuracy = {0}, train accuracy = {1}'.format(train_acc, test_acc))
+
+# Evaluate linear support vector classification 
+svc_model_eval = evaluate_model(svc_model, X_train, y_train, X_test, y_test, model_name_str='Support vector machine classification')
+
+# %% Learning Curves 
 
 # %% 
 # Output prediction and save model
 output_kaggle_prediction_and_save_model(svc_model, X_final_test_enc, final_idxs)
+
 # %%
+# Evaluating difference between using labels or probabilities to calculate AUC
+with open( '{0}'.format(models_path/'2021-10-24-19:59:39-submission.pkl'),'rb') as f:
+    log_model = pickle.load(f)
+
+def evaluate_model_proba(model, X_train, y_train, X_test, y_test, model_name_str='', should_print=True):
+    # Evaluates model in terms of test and train accuracy 
+    # Train data evaluation
+    y_train_pred = model.predict_proba(X_train)[:,1]
+    train_acc = model.score(X_train, y_train)
+    train_auc = metrics.roc_auc_score(y_train, y_train_pred)
+
+    # Test data evaluation
+    y_test_pred = model.predict_proba(X_test)[:,1]
+    test_acc = model.score(X_test, y_test)
+    test_auc = metrics.roc_auc_score(y_test, y_test_pred)
+
+    if should_print:
+        print('{0} model: train accuracy = {1}, train AUC = {2} \ntest accuracy = {3}, test AUC = {4}'.format(model_name_str, train_acc, train_auc, test_acc, test_auc))
+    return train_acc, train_auc, test_acc, test_auc
+
+log_model_eval = evaluate_model(log_model, X_train, y_train, X_test, y_test, model_name_str=log_model_name_str)
+log_model_eval = evaluate_model_proba(log_model, X_train, y_train, X_test, y_test, model_name_str=log_model_name_str)
+# I assume if probabilities are given to kaggle submission then will round up or down to get label prediction... (since AUC of using proba is not close to what kaggles AUC is while if label prediction is used is close to kaggles AUC)
