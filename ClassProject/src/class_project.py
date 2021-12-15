9a11ff0ce3ce8c54627dc35c130b9f498929b6fc
Ryan Dalby <dalbyryan3@gmail.com>
2021-12-14 17:38:21 -0700

    Implemented gradient boosting, MLP, and bagged classifiers

--- ClassProject/src/class_project.py
+++ ClassProject/src/class_project.py
@@ -4,12 +4,18 @@ from datetime import datetime
 import numpy as np
 import pandas as pd
 from pathlib import Path
+import itertools
+import matplotlib.pyplot as plt
+
 from sklearn import preprocessing
 from sklearn import linear_model
 from sklearn import model_selection
 from sklearn import metrics
 from sklearn import svm
-import matplotlib.pyplot as plt
+from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import HistGradientBoostingClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import GridSearchCV
 
 # %% Config variables
 data_root_path = Path('../data')
@@ -18,7 +24,7 @@ models_path = Path('../models')
 
 # %% General functions
 def output_kaggle_prediction_and_save_model(model, X_final_test, final_idxs):
-    y_final = model.predict(X_final_test)
+    y_final = model.predict_proba(X_final_test)[:,1]
     final_test_pred_df = pd.DataFrame({'ID':final_idxs, 'Prediction':y_final})
     filename = datetime.now().strftime('%Y-%m-%d-%H:%M:%S-submission')
     final_test_pred_df.to_csv(kaggle_submissions_path / '{0}'.format(filename+'.csv'), index=False)
@@ -29,7 +35,6 @@ def output_kaggle_prediction_and_save_model(model, X_final_test, final_idxs):
 def load_model(filename):
     with open(models_path / '{0}'.format(filename+'.pkl'),'rb') as f:
         return pickle.load(f)
-
 
 def encode_data(X, categorical_cols_idxs, enc):
     X_categorical = X[:,categorical_cols_idxs]
@@ -42,13 +47,15 @@ def evaluate_model(model, X_train, y_train, X_test, y_test, model_name_str='', s
     # Evaluates model in terms of test and train accuracy 
     # Train data evaluation
     y_train_pred = model.predict(X_train)
+    y_train_pred_prob = model.predict_proba(X_train)[:,1]
     train_acc = metrics.accuracy_score(y_train, y_train_pred)
-    train_auc = metrics.roc_auc_score(y_train, y_train_pred)
+    train_auc = metrics.roc_auc_score(y_train, y_train_pred_prob)
 
     # Test data evaluation
     y_test_pred = model.predict(X_test)
+    y_test_pred_prob = model.predict_proba(X_test)[:,1]
     test_acc = metrics.accuracy_score(y_test, y_test_pred)
-    test_auc = metrics.roc_auc_score(y_test, y_test_pred)
+    test_auc = metrics.roc_auc_score(y_test, y_test_pred_prob)
 
     if should_print:
         print('{0} model: train accuracy = {1}, train AUC = {2} \ntest accuracy = {3}, test AUC = {4}'.format(model_name_str, train_acc, train_auc, test_acc, test_auc))
@@ -70,6 +77,8 @@ def create_learning_curves(model, X, y, num_data_points=5, model_name_str=''):
     plt.show()
     return avg_train_scores, avg_val_scores 
 # %%
+###
+# %%
 # Load path 
 loaded_train_data = pd.read_csv(data_root_path / 'train_final.csv')
 loaded_test_data = pd.read_csv(data_root_path / 'test_final.csv')
@@ -78,6 +87,8 @@ y_full_train = loaded_train_data.iloc[:,-1].to_numpy()
 X_final_test = loaded_test_data.iloc[:,1:].to_numpy()
 final_idxs = loaded_test_data.iloc[:,0].to_numpy()
 
+# %%
+###
 # %%
 # Data preprocessing 
 is_categorical_data = (loaded_train_data.dtypes == 'object')
@@ -145,3 +156,100 @@ with open( '{0}'.format('svc_learning_curve.pkl'),'wb') as f:
 # %% 
 # Output prediction and save model
 output_kaggle_prediction_and_save_model(svc_model, X_final_test_enc_norm, final_idxs)
+
+# %%
+###
+# %%
+# Logistic regression bagging model
+log_bag_model = BaggingClassifier(linear_model.LogisticRegression(solver='lbfgs', max_iter=500), max_samples=1.0, max_features=1.0, bootstrap=True)
+log_bag_model.fit(X_train, y_train)
+log_bag_model_name_str = 'Bagged Logistic Regression Classification'
+
+log_bag_model_eval = evaluate_model(log_bag_model, X_train, y_train, X_test, y_test, model_name_str=log_bag_model)
+
+# %% Learning Curves 
+log_bag_model_train_learning_curve, log_bag_model_val_learning_curve = create_learning_curves(log_bag_model, X_train, y_train, model_name_str=log_bag_model_name_str, num_data_points=10)
+
+# %% Save learning curves 
+with open( '{0}'.format('log_bag_learning_curve.pkl'),'wb') as f:
+    pickle.dump([log_bag_model_train_learning_curve, log_bag_model_val_learning_curve],f)
+
+# %% 
+# Output prediction and save model
+output_kaggle_prediction_and_save_model(log_bag_model, X_final_test_enc_norm, final_idxs)
+
+# %%
+###
+
+# %%
+hidden_layer_size_list = [(100,50)]
+learning_rate_init_list = [0.05]
+alpha_list = [0.005]
+batch_size_list = [256, 512]
+
+best_test_auc = 0
+best_params = ()
+
+for hl, lr, alpha, bs in itertools.product(hidden_layer_size_list, learning_rate_init_list, alpha_list, batch_size_list):
+    mlp_model = MLPClassifier(hidden_layer_sizes=hl, activation='relu', solver='adam', alpha=alpha, batch_size=bs, learning_rate_init=lr, max_iter=300, early_stopping=True)
+    mlp_model.fit(X_train, y_train)
+    mlp_model_name_str = 'Fully Connected Neural Network (MLP) Classifier'
+    print('Hidden layer shape of {0}, learning rate of {1}, alpha of {2}, and batch size of {3}:'.format(hl, lr, alpha, bs))
+    mlp_model_eval = evaluate_model(mlp_model, X_train, y_train, X_test, y_test, model_name_str=mlp_model_name_str)
+    _,_,_,test_auc = mlp_model_eval
+    print('-----------')
+    print()
+    if test_auc > best_test_auc:
+        best_test_auc =  test_auc
+        best_params = (hl, lr, alpha, bs)
+print('Best test auc is {0} from hidden layer shape of {1}, learning rate of {2}, alpha of {3}, and batch size of {4}'.format(best_test_auc, *best_params))
+
+# %%
+mlp_model = MLPClassifier(hidden_layer_sizes=(100,50), activation='relu', solver='adam', alpha=0.0005, batch_size=128, learning_rate_init=0.008, max_iter=300, early_stopping=True)
+mlp_model.fit(X_train, y_train)
+mlp_model_name_str = 'Fully Connected Neural Network (MLP) Classifier'
+mlp_model_eval = evaluate_model(mlp_model, X_train, y_train, X_test, y_test, model_name_str=mlp_model_name_str)
+
+# %% 
+# Output prediction and save model
+output_kaggle_prediction_and_save_model(mlp_model, X_final_test_enc_norm, final_idxs)
+
+# %%
+###
+# %%
+# Histogram Gradient Boosting Classifier  
+learning_rate = [0.01, 0.1, 0.15]
+param_grid = [
+    {'learning_rate':[0.07, 0.1], 'max_depth':[31,61], 'l2_regularization':[1, 15, 30], 'max_leaf_nodes':[15], 'min_samples_leaf':[10,30]}
+]
+
+hist_grad_boost_model_grid_search = HistGradientBoostingClassifier(loss='binary_crossentropy', max_iter=200, early_stopping=True)
+hist_grad_boost_grid_search = GridSearchCV(hist_grad_boost_model_grid_search, param_grid, scoring='roc_auc', n_jobs=-1)
+hist_grad_boost_grid_search_results = hist_grad_boost_grid_search.fit(X_train, y_train)
+
+# %%
+print(hist_grad_boost_grid_search_results.best_params_)
+
+# %%
+hist_grad_boost_model = HistGradientBoostingClassifier(loss='binary_crossentropy', learning_rate=0.1, max_iter=200, max_depth=31, l2_regularization=15, early_stopping=True, max_leaf_nodes=15, min_samples_leaf=10)
+hist_grad_boost_model.fit(X_train, y_train)
+hist_grad_boost_model_name_str = 'Histogram Based Gradient Boosting Classifier'
+
+hist_grad_boost_model_eval = evaluate_model(hist_grad_boost_model, X_train, y_train, X_test, y_test, model_name_str=hist_grad_boost_model_name_str)
+
+# %% Learning Curves 
+hist_grad_boost_model_train_learning_curve, hist_grad_boost_model_val_learning_curve = create_learning_curves(hist_grad_boost_model, X_train, y_train, model_name_str=hist_grad_boost_model_name_str, num_data_points=10)
+
+# %% Save learning curves 
+with open( '{0}'.format('grad_boost_learning_curve.pkl'),'wb') as f:
+    pickle.dump([hist_grad_boost_model_train_learning_curve, hist_grad_boost_model_val_learning_curve],f)
+
+# %% 
+# Output prediction and save model
+output_kaggle_prediction_and_save_model(hist_grad_boost_model, X_final_test_enc_norm, final_idxs)
+
+
+# %%
+###
+
+# %%
